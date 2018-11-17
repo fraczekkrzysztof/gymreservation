4f3e80f26e6a664ab92aa3168a3bc4236aa213dc
fraczekkrzysztof <fraczekkrzysztof@o2.pl>
2018-11-17 20:30:21 +0100

    first commit

---
+++ src/main/java/com/fraczekkrzysztof/gymreservation/GymreservationApplication.java
@@ -0,0 +1,12 @@
+package com.fraczekkrzysztof.gymreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymreservationApplication.class, args);
+	}
+}
